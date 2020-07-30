3b5601a21f296bc021c415fef3338d85eee85de1
JesusMoraDaw <jmora@kodemic.com>
2020-07-30 13:31:13 +0200

    'searchfilter_working'

--- src/app/components/owner-list/owner-list.component.ts
+++ src/app/components/owner-list/owner-list.component.ts
@@ -4,6 +4,7 @@ import { CounterCatService } from 'src/core/services/counter-cat.service';
 import { Subscription } from 'rxjs';
 import { CounterFavService } from 'src/core/services/counter-fav.service';
 import { DataFavService } from 'src/core/services/data-fav.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-owner-list',
@@ -25,8 +26,13 @@ export class OwnerListComponent implements OnInit {
   private finishPage: number;
   private actualPage: number;
 
+  public search: boolean;
+  public filterSearch;
+  public url;
+
   constructor(
     private _ownerService: OwnersService,
+    private _route: ActivatedRoute,
     private _counterCatService: CounterCatService,
     private _counterFavService: CounterFavService,
     private _dataFavService: DataFavService,
@@ -34,7 +40,10 @@ export class OwnerListComponent implements OnInit {
 
   ngOnInit() {
     this.initVariables();
+    this.searchBar();
     this.getOrderList(this.actualPage);
+
+
   }
 
   public initVariables(){
@@ -43,11 +52,13 @@ export class OwnerListComponent implements OnInit {
     this.errorMessage = "";
     this.loading = true;
 
-
     this.subcripcionCat = this._counterCatService.getContadorMatagatos().subscribe(counter => {this.countCat = counter});
     this.subcripcionFav = this._counterFavService.getContadorFavoritos().subscribe(counter => {this.countFav = counter});
 
     this.actualPage = 1;
+
+    this.search = false;
+    this.filterSearch = '';
 
   }
   public getOrderList(actualPage) {
@@ -101,6 +112,33 @@ export class OwnerListComponent implements OnInit {
 
   }
 
+  public onSubmit(filterSearch){
+
+    this._counterCatService.setContadorMatagatos(this.incrementCat());
+
+    this.filterSearch = filterSearch;
+
+    this._ownerService.getSearchDetail(this.filterSearch, this.actualPage).subscribe(
+      (response: any) => {
+        if (response) {
+          //console.log(response);
+          this.ownerList = response.result;
+          this.finishPage = response._meta.pageCount;
+
+          this.loading = false;
+        } else {
+          this.loading = false;
+        }
+      },
+      (error) => {
+        this.errorMessage = <any>error;
+        if (this.errorMessage != null) {
+        }
+        console.log(this.errorMessage);
+      }
+    );
+  }
+
   public incrementCat(){
     this.countCat +=1;
     return this.countCat;
@@ -119,11 +157,28 @@ export class OwnerListComponent implements OnInit {
   public showMore(){
     if (this.actualPage < this.finishPage) {
       this.actualPage ++;
-      this.getOrderList(this.actualPage);
+      if(this.search){
+        this.onSubmit(this.filterSearch);
+        this.actualPage = 1;
+      }
+      else{
+        this.getOrderList(this.actualPage);
+      }
 
     } else {
       console.log('No more lines. Finish page!');
     }
   }
 
+  public searchBar(){
+    this._route.url.subscribe(data => this.url = data);
+    this.url.forEach(element => {
+      if(element.path == 'search'){
+        this.search = true;
+      }
+      else{
+        this.search = false;
+      }
+    });
+  }
 }
